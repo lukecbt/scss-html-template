044eccc0bfb3cbf25c9558172e4de65bb1152352
Luke <lukebeatty96@gmail.com>
2022-06-26 19:14:44 +0100

    Added hover animation for card header

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 /* Hamburger Menu */
 
-var hamburger = document.getElementsByClassName("hamburger")[0]
+var hamburger = document.getElementsByClassName('hamburger')[0]
 
-hamburger.addEventListener("click", () => {
+hamburger.addEventListener('click', () => {
     console.log('hamburger')
 });
 
@@ -11,7 +11,7 @@ hamburger.addEventListener("click", () => {
 /* Scroll Animation */
 
 function reveal() {
-    var reveals = document.querySelectorAll(".reveal");
+    var reveals = document.querySelectorAll('.reveal');
   
     for (var i = 0; i < reveals.length; i++) {
       var windowHeight = window.innerHeight;
@@ -19,13 +19,40 @@ function reveal() {
       var elementVisible = 150;
   
       if (elementTop < windowHeight - elementVisible) {
-        reveals[i].classList.add("active");
+        reveals[i].classList.add('active');
       } else {
-        reveals[i].classList.remove("active");
+        reveals[i].classList.remove('active');
       }
     }
   }
   
-  window.addEventListener("scroll", reveal);
+  window.addEventListener('scroll', reveal);
   
 /* End Scroll Animation */
+
+/* Card Header Hover Animation */
+
+var hoverColor = 'rgb(95, 141, 193)';
+
+var cards = document.querySelectorAll('.card');
+
+cards.forEach(element => {
+  element.addEventListener('mouseenter', (e) =>  {
+    var cardHeader = e.target.querySelector('.card__header');
+    if (cardHeader) {
+      cardHeader.style.transition = '0.3s';
+      cardHeader.style.color = hoverColor;
+    }
+  });
+});
+
+cards.forEach(element => {
+  element.addEventListener('mouseleave', (e) =>  {
+    var cardHeader = e.target.querySelector('.card__header');
+    if (cardHeader) {
+      cardHeader.style.color = 'unset';
+    }
+  });
+});
+
+/* Card Hover Animation End */
